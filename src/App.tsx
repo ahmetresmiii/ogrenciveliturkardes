fddd5d8e043ba70a4b84aa584922041ea98ab162
Ahmet Mesrur Şahin <ahmetmesrurresmi0@gmail.com>
2026-07-10 20:48:24 +0300

    Remove duplicate Firebase config in App.tsx
    Removed duplicate Firebase configuration code.

--- src/App.tsx
+++ src/App.tsx
@@ -51,20 +51,6 @@ const firebaseConfig = {
   measurementId: "G-XX6TJVT61R"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// --- FIREBASE CONFIGURATION ---
-const firebaseConfig = {
-  apiKey: "AIzaSyDs5blACfmlwuFmiiF5QjqQW-6d33I4hU0",
-  authDomain: "ogrencivelibaki.firebaseapp.com",
-  projectId: "ogrencivelibaki",
-  storageBucket: "ogrencivelibaki.firebasestorage.app",
-  messagingSenderId: "466518686663",
-  appId: "1:466518686663:web:a2d3c7f95b4e86710677e2",
-  measurementId: "G-XX6TJVT61R"
-};
-
 // Initialize Firebase App & Firestore Database
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
